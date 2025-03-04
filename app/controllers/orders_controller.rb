acfd6874b6871c2fb88b5da093b81396a6fdb4e4
mayumishibata-div <mayumi.shibata@di-v.co.jp>
2025-03-04 18:00:22 +0900

    エラーメッセージの確認
    @order.valid?
    =false
    @order.errors.full_messages
    =["Price can't be blank"]

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,6 +5,7 @@ class OrdersController < ApplicationController
 
   def create
     @order = Order.new(order_params)
+    binding.pry
     if @order. valid? #この注文は正しいかな？
       @order.save #注文が正しければ、その注文を保存
       return redirect_to root_path 
