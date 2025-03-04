7a0827776ae5c1d64c04649d5aa6fbd805689c20
mayumishibata-div <mayumi.shibata@di-v.co.jp>
2025-03-04 18:00:59 +0900

    エラーメッセージが確認できたので削除

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,7 +5,6 @@ class OrdersController < ApplicationController
 
   def create
     @order = Order.new(order_params)
-    binding.pry
     if @order. valid? #この注文は正しいかな？
       @order.save #注文が正しければ、その注文を保存
       return redirect_to root_path 
