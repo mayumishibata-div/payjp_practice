be278440e743f54c2085bd57d12724de1db4fc62
mayumishibata-div <mayumi.shibata@di-v.co.jp>
2025-03-04 17:43:49 +0900

    投稿した内容がDBに保存されるように定義

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,20 @@
 class OrdersController < ApplicationController
   def index
-
+    @order = Order.new
   end
 
   def create
-    
+    @order = Order.new(order_params)
+    if @order. valid? #この注文は正しいかな？
+      @order.save #注文が正しければ、その注文を保存
+      return redirect_to root_path 
+    else
+      render "index", status: :unprocessable_entity
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:price)
   end
 end
