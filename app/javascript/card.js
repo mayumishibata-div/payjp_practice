43f0f47cf5304602e8be9f950ed583f4ec338007
mayumishibata-div <mayumi.shibata@di-v.co.jp>
2025-03-05 17:44:20 +0900

    公開かぎの設定

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,4 +1,5 @@
 const pay = () => {
+  const payjp = Payjp('pk_test_***********************')// PAY.JPテスト公開鍵
   const form = document.getElementById('charge-form')
   form.addEventListener("submit", (e) => {
     console.log("フォーム送信時にイベント発火")
