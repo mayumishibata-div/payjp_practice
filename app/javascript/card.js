09f365c9ac2476249fed41b2b046f3e44a1e103c
mayumishibata-div <mayumi.shibata@di-v.co.jp>
2025-03-05 17:29:30 +0900

    フォームの送信ボタンをクリックしたら、イベントが発火するように実装

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,10 @@
 const pay = () => {
-  console.log("カード情報トークン化のためのJavaScript");
+  const form = document.getElementById('charge-form')
+  form.addEventListener("submit", (e) => {
+    console.log("フォーム送信時にイベント発火")
+    e.preventDefault();
+    // e.preventDefault();で通常のRuby on Railsにおけるフォーム送信処理はキャンセルされています。
+  });
 };
 
 window.addEventListener("turbo:load", pay);
