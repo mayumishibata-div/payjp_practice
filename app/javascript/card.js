8a42cd0ab6ba3c8c0a1664d0f71398cc9ae83d79
mayumishibata-div <mayumi.shibata@di-v.co.jp>
2025-03-06 10:29:30 +0900

    PAY.JPのライブラリを使い、HTML要素をフォームと置き換える

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,13 @@
 const pay = () => {
   const payjp = Payjp('pk_test_***********************')// PAY.JPテスト公開鍵
+  const elements = payjp.elements();  //入力の部品を作る準備
+  const numberElement = elements.create('cardNumber'); //「カード番号」を入力する場所の部品を作る
+  const expiryElement = elements.create('cardExpiry'); //「カードの有効期限」を入力する場所の部品を作る
+  const cvcElement = elements.create('cardCvc');  //「CVC」という番号を入力する場所を作る
+
+  numberElement.mount('#number-form');//#number-form」という部分にくっつける
+  expiryElement.mount('#expiry-form');//「#expiry-form」という部分にくっつけているよ。
+  cvcElement.mount('#cvc-form');//「#cvc-form」というところにくっつけているんだ。
   const form = document.getElementById('charge-form')
   form.addEventListener("submit", (e) => {
     console.log("フォーム送信時にイベント発火")
