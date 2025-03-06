60b6e82decc6f1febf7ba7b56e0896bc31fa70c8
mayumishibata-div <mayumi.shibata@di-v.co.jp>
2025-03-06 11:06:04 +0900

    Revert "トークン化の処理"
    This reverts commit fa59cd0819b0fc1f6b4e3fb5c48e46ac22a6803e.

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  const payjp = Payjp('pk_test_ca69e7809393e67ab18301d5')// PAY.JPテスト公開鍵
+  const payjp = Payjp('pk_test_***********************')// PAY.JPテスト公開鍵
   const elements = payjp.elements();  //入力の部品を作る準備
   const numberElement = elements.create('cardNumber'); //「カード番号」を入力する場所の部品を作る
   const expiryElement = elements.create('cardExpiry'); //「カードの有効期限」を入力する場所の部品を作る
@@ -10,19 +10,6 @@ const pay = () => {
   cvcElement.mount('#cvc-form');//「#cvc-form」というところにくっつけているんだ。
   const form = document.getElementById('charge-form')
   form.addEventListener("submit", (e) => {
-    //ユーザーが入力したカード情報から、安全に使える「トークン」という特別な数字を作るためのコード
-    payjp.createToken(numberElement).then(function (response) {
-       //createToken の処理が終わった後、結果（response）が返ってきたときに実行される部分
-      if (response.error) {
-        //エラーがあったら
-      } else {
-        // エラーがなかったら
-        const token = response.id;
-        console.log(token)
-      }
-    });
-
-
     console.log("フォーム送信時にイベント発火")
     e.preventDefault();
     // e.preventDefault();で通常のRuby on Railsにおけるフォーム送信処理はキャンセルされています。
